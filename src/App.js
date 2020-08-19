92fdf3ea27ba9b6a1ff83f107e3e3b6f273952ca
beguvoid <bilguunsoninbayar@gmail.com>
2020-08-20 01:49:40 +0800

    Tried working on Frontside Fetching

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ import RecommendedVideos from "./RecommendedVideos";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="app">
-            <Router>
-                <Switch>
-                    <Route path="/Login">
-                        <Login />
-                    </Route>
-                    <Route path="/">
-                        <Header />
-                        <div className="app_page">
-                            <Sidebar />
-                            <RecommendedVideos />
-                        </div>
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <div className="app_page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
-    return (
-        <div className="login">
-            <input className="idNumber" placeholder="Id Number" />
-            <input
-                placeholder="Password"
-                secureTextEntry="true"
-                type="password"
-            />
-            <Link to="/">
-                <button>Login</button>
-            </Link>
-        </div>
-    );
+  // POST REQ to localhost:5000/users/login
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:5000/users/login", {
+      method: "POST",
+      body: e,
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="idNumber"
+          value={username}
+          onChange={onUsernameChange}
+          placeholder="Username"
+        />
+        <input
+          placeholder="Password"
+          value={password}
+          onChange={onPasswordChange}
+          type="password"
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
